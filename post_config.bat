34910269c49a1cc2223097ed56c6d21fd7722c05
Gary Ritchie <gary@lyonritchie.com>
2026-07-25 13:13:35 -0400

    Fix: post_config script for Windows

--- post_config.bat
+++ post_config.bat
@@ -1,9 +1,11 @@
 @echo off
-:: Auto-generated post_config script
+:: Manually edited post_config script for Windows
 
 echo Running: gitignore
-cat *.gitignore > .gitignore && rm *.gitignore
+type *.gitignore > .gitignore.tmp 2>nul & del *.gitignore 2>nul & ren .gitignore.tmp .gitignore
+
 echo Running: Initialize git repository
 git init
+
 echo Running: Install git-lfs hooks
 git lfs install
